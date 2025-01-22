f117f240a7785ab386c7cb33e6926cb953d15ca9
mguthriem <guthriem@ornl.gov>
2025-01-22 16:47:50 -0500

    added script to run reduction from workbench editor

---
+++ userScript.py
@@ -0,0 +1,10 @@
+import sys
+import importlib
+sys.path.append("/SNS/SNAP/shared/Malcolm/code/SNAPBlue")
+import blueUtils as blue
+importlib.reload(blue)
+
+#to do a simple reduction of run 61991
+
+blue.reduceSNAP(61991) 
+
